97acf1fded21d63cb79d32238342f4dfc66b640e
Leonard Chinonso <leokingluthers@gmail.com>
2021-07-02 21:44:00 +0100

    Break: committing for later changes

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+from app import app
+import unittest
+
+
+class TestTest(unittest.TestCase):
+
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get("/block/latest", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
